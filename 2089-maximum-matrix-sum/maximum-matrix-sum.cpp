039f54db1baad1958a693745b6dae638a6cf0e9b
Abhinandan <115779184+abhi07cemk@users.noreply.github.com>
2026-01-05 09:47:04 +0530

    Time: 0 ms (100.00%) | Memory: 39.2 MB (25.94%) - LeetSync

---
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        long long sum = 0;
+        int negCount = 0;
+        int minAbs = INT_MAX;
+
+        for (auto &row : matrix) {
+            for (int val : row) {
+                if (val < 0)
+                    negCount++;
+
+                sum += llabs(val);
+                minAbs = min(minAbs, abs(val));
+            }
+        }
+
+        // If odd number of negatives, one smallest abs value stays negative
+        if (negCount % 2 == 1) {
+            sum -= 2LL * minAbs;
+        }
+
+        return sum;
+    }
+};
