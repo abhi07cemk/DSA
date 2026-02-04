38f39a4ecf12b07a62e448128b71698290e2f671
Abhinandan <115779184+abhi07cemk@users.noreply.github.com>
2026-02-04 20:12:11 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (62.64%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -18,3 +18,26 @@ public:
         return candidate;
     }
 };
+
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+
+//         for (int i = 0; i < n; i++) {
+//             int count = 0;
+
+//             for (int j = 0; j < n; j++) {
+//                 if (nums[i] == nums[j]) {
+//                     count++;
+//                 }
+//             }
+
+//             if (count > n / 2) {
+//                 return nums[i];
+//             }
+//         }
+//         return -1; // will never be reached as majority element always exists
+//     }
+// };
