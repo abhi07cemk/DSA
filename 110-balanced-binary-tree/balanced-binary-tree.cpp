dfe884c0436fb6f9668a80a89561da6116bce240
Abhinandan <115779184+abhi07cemk@users.noreply.github.com>
2026-02-08 05:49:58 +0530

    Time: 71 ms (5.10%) | Memory: 23.3 MB (20.68%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -9,62 +9,62 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// private:
-//     int height(TreeNode* node){
-//         //base case
-//         if(node == NULL) {
-//             return 0;
-//         }
-        
-//         int left = height(node ->left);
-//         int right = height(node->right);
-        
-//         int ans = max(left, right) + 1;
-//         return ans;
-        
-//     }
-// public:
-//     bool isBalanced(TreeNode* root) {
-//         if(root == NULL)
-//         {
-//             return true;
-//         }
-
-//         bool left = isBalanced(root -> left);
-//         bool right = isBalanced(root -> right);
-
-//         bool diff = abs(height(root -> left) - height(root -> right)) <= 1;
-
-//         if(left && right && diff)
-//         {
-//             return 1;
-//         }
-//         else
-//         {
-//             return false;
-//         }
-//     }
-// };
-
 class Solution {
 private:
-    int height(TreeNode* root) {
-        if (root == NULL) return 0;
-
-        int left = height(root->left);
-        if (left == -1) return -1;
-
-        int right = height(root->right);
-        if (right == -1) return -1;
-
-        if (abs(left - right) > 1) return -1;
-
-        return max(left, right) + 1;
+    int height(TreeNode* node){
+        //base case
+        if(node == NULL) {
+            return 0;
+        }
+        
+        int left = height(node ->left);
+        int right = height(node->right);
+        
+        int ans = max(left, right) + 1;
+        return ans;
+        
     }
-
 public:
     bool isBalanced(TreeNode* root) {
-        return height(root) != -1;
+        if(root == NULL)
+        {
+            return true;
+        }
+
+        bool left = isBalanced(root -> left);
+        bool right = isBalanced(root -> right);
+
+        bool diff = abs(height(root -> left) - height(root -> right)) <= 1;
+
+        if(left && right && diff)
+        {
+            return 1;
+        }
+        else
+        {
+            return false;
+        }
     }
 };
+
+// class Solution {
+// private:
+//     int height(TreeNode* root) {
+//         if (root == NULL) return 0;
+
+//         int left = height(root->left);
+//         if (left == -1) return -1;
+
+//         int right = height(root->right);
+//         if (right == -1) return -1;
+
+//         if (abs(left - right) > 1) return -1;
+
+//         return max(left, right) + 1;
+//     }
+
+// public:
+//     bool isBalanced(TreeNode* root) {
+//         return height(root) != -1;
+//     }
+// };
