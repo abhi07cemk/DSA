607a97471162bfa95027849b375d884b9b28b3f7
Abhinandan <115779184+abhi07cemk@users.noreply.github.com>
2026-02-09 11:19:00 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (16.53%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,17 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool helper(TreeNode* root, long long minVal, long long maxVal) {
-        if (root == NULL)
-            return true;
+    bool isBST(TreeNode* root, long min, long max)
+    {
+        if (root == NULL) return true;
 
-        if (root->val <= minVal || root->val >= maxVal)
+        if (root->val > min && root->val < max)
+        {
+            bool left = isBST(root->left, min, root->val);
+            bool right = isBST(root->right, root->val, max);
+            return left && right;
+        }
+        else
+        {
             return false;
-
-        return helper(root->left, minVal, root->val) &&
-               helper(root->right, root->val, maxVal);
+        }
     }
 
     bool isValidBST(TreeNode* root) {
-        return helper(root, LLONG_MIN, LLONG_MAX);
+        return isBST(root, LONG_MIN, LONG_MAX);
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool helper(TreeNode* root, long long minVal, long long maxVal) {
+//         if (root == NULL)
+//             return true;
+
+//         if (root->val <= minVal || root->val >= maxVal)
+//             return false;
+
+//         return helper(root->left, minVal, root->val) &&
+//                helper(root->right, root->val, maxVal);
+//     }
+
+//     bool isValidBST(TreeNode* root) {
+//         return helper(root, LLONG_MIN, LLONG_MAX);
+//     }
+// };
