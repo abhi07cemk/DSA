1eb37ffae10c3ea79b696b2361948d4cd16906eb
Abhinandan <115779184+abhi07cemk@users.noreply.github.com>
2026-02-15 11:10:57 +0530

    Time: 3 ms (62.88%) | Memory: 10.4 MB (46.77%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,15 +1,77 @@
+// class Solution {
+// public:
+    
+//     void solve(int col,
+//                vector<string> &board,
+//                vector<vector<string>> &ans,
+//                vector<int> &leftRow,
+//                vector<int> &upperDiagonal,
+//                vector<int> &lowerDiagonal,
+//                int n) 
+//     {
+//         // Base case
+//         if(col == n) {
+//             ans.push_back(board);
+//             return;
+//         }
+        
+//         for(int row = 0; row < n; row++) {
+            
+//             // Check if safe using hashing (O(1))
+//             if(leftRow[row] == 0 && 
+//                lowerDiagonal[row + col] == 0 &&
+//                upperDiagonal[n - 1 + col - row] == 0) {
+                
+//                 // Place queen
+//                 board[row][col] = 'Q';
+//                 leftRow[row] = 1;
+//                 lowerDiagonal[row + col] = 1;
+//                 upperDiagonal[n - 1 + col - row] = 1;
+                
+//                 solve(col + 1, board, ans, leftRow, upperDiagonal, lowerDiagonal, n);
+                
+//                 // Backtrack
+//                 board[row][col] = '.';
+//                 leftRow[row] = 0;
+//                 lowerDiagonal[row + col] = 0;
+//                 upperDiagonal[n - 1 + col - row] = 0;
+//             }
+//         }
+//     }
+    
+//     vector<vector<string>> solveNQueens(int n) {
+        
+//         vector<vector<string>> ans;
+        
+//         vector<string> board(n);
+//         string s(n, '.');
+//         for(int i = 0; i < n; i++) {
+//             board[i] = s;
+//         }
+        
+//         vector<int> leftRow(n, 0);
+//         vector<int> upperDiagonal(2*n - 1, 0);
+//         vector<int> lowerDiagonal(2*n - 1, 0);
+        
+//         solve(0, board, ans, leftRow, upperDiagonal, lowerDiagonal, n);
+        
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     
     void solve(int col,
                vector<string> &board,
                vector<vector<string>> &ans,
-               vector<int> &leftRow,
-               vector<int> &upperDiagonal,
-               vector<int> &lowerDiagonal,
+               unordered_map<int, bool> &rowMap,
+               unordered_map<int, bool> &lowerDiag,
+               unordered_map<int, bool> &upperDiag,
                int n) 
     {
-        // Base case
+        // Base Case
         if(col == n) {
             ans.push_back(board);
             return;
@@ -17,24 +79,24 @@ public:
         
         for(int row = 0; row < n; row++) {
             
-            // Check if safe using hashing (O(1))
-            if(leftRow[row] == 0 && 
-               lowerDiagonal[row + col] == 0 &&
-               upperDiagonal[n - 1 + col - row] == 0) {
+            // Check if safe
+            if(!rowMap[row] &&
+               !lowerDiag[row + col] &&
+               !upperDiag[n - 1 + col - row]) {
                 
-                // Place queen
+                // Place Queen
                 board[row][col] = 'Q';
-                leftRow[row] = 1;
-                lowerDiagonal[row + col] = 1;
-                upperDiagonal[n - 1 + col - row] = 1;
+                rowMap[row] = true;
+                lowerDiag[row + col] = true;
+                upperDiag[n - 1 + col - row] = true;
                 
-                solve(col + 1, board, ans, leftRow, upperDiagonal, lowerDiagonal, n);
+                solve(col + 1, board, ans, rowMap, lowerDiag, upperDiag, n);
                 
                 // Backtrack
                 board[row][col] = '.';
-                leftRow[row] = 0;
-                lowerDiagonal[row + col] = 0;
-                upperDiagonal[n - 1 + col - row] = 0;
+                rowMap[row] = false;
+                lowerDiag[row + col] = false;
+                upperDiag[n - 1 + col - row] = false;
             }
         }
     }
@@ -42,18 +104,13 @@ public:
     vector<vector<string>> solveNQueens(int n) {
         
         vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
         
-        vector<string> board(n);
-        string s(n, '.');
-        for(int i = 0; i < n; i++) {
-            board[i] = s;
-        }
+        unordered_map<int, bool> rowMap;
+        unordered_map<int, bool> lowerDiag;
+        unordered_map<int, bool> upperDiag;
         
-        vector<int> leftRow(n, 0);
-        vector<int> upperDiagonal(2*n - 1, 0);
-        vector<int> lowerDiagonal(2*n - 1, 0);
-        
-        solve(0, board, ans, leftRow, upperDiagonal, lowerDiagonal, n);
+        solve(0, board, ans, rowMap, lowerDiag, upperDiag, n);
         
         return ans;
     }
